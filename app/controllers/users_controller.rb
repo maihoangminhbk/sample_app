836cc54cc3b1a65fa552a4f4900c456b9daed246
Minh Mai <maihoangminhbk@gmail.com>
2022-08-09 09:31:58 +0700

    Chapter 8: Basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = t("global.flash.user.success")
       redirect_to @user
     else
